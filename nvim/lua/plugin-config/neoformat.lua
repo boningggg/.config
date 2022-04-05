bb56e0648a684b86e23895eb0320f2c5cad422f9
boningggg <boningggg@boninggggs-MBP.lan>
2022-04-05 12:35:43 +0800

    [nvim] add clang-format support for java

--- nvim/lua/plugin-config/neoformat.lua
+++ nvim/lua/plugin-config/neoformat.lua
@@ -1,10 +1,24 @@
 vim.g.neoformat_c_clangformat = {
   exe = "clang-format",
   args = {
+    "-assume-filename=" .. vim.fn.expand("%:t"),
     "-style='{BasedOnStyle: GNU, \
               IndentWidth: 8, \
               AlignConsecutiveDeclarations: true}'"
-  }
+  },
+  stdin = 1
 }
 vim.g.neoformat_enabled_c = { "clangformat" }
+
+vim.g.neoformat_java_clangformat = {
+  exe = "clang-format",
+  args = {
+    "-assume-filename=" .. vim.fn.expand("%:t"),
+    "-style='{BasedOnStyle: LLVM, \
+              IndentWidth: 4, \
+              AlignConsecutiveDeclarations: true}'"
+  },
+  stdin = 1
+}
+vim.g.neoformat_enabled_java = { "clangformat" }
 
