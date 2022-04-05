db628f85b5536ef4c6cb3a26cc32a42b5a5580c9
boningggg <boningggg@boninggggs-MBP.lan>
2022-04-05 17:46:11 +0800

    [nvim] change the style of lsp floating windows of hover and diagnostic

--- nvim/lua/plugin-config/nvim-lspconfig.lua
+++ nvim/lua/plugin-config/nvim-lspconfig.lua
@@ -1,3 +1,4 @@
+---------- Server Config ----------
 local servers = {
   clangd = require("lsp.clangd"), -- for C
   jdtls = require("lsp.jdtls"), -- for Java
@@ -12,3 +13,30 @@ for name, opts in pairs(servers) do
   opts.capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
   require('lspconfig')[name].setup(opts)
 end
+
+---------- Lsp Floating Window Config ----------
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, { border = "rounded" }
+)
+
+local signs = { 
+  DiagnosticSignError = "", 
+  DiagnosticSignWarn = "", 
+  DiagnosticSignHint = "", 
+  DiagnosticSignInfo = "" 
+}
+
+for hl, icon in pairs(signs) do
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  update_in_insert = true,
+  severity_sort = true,
+  float = {
+    header = "",
+    border = "rounded"
+  }
+})
