83a07503b105073c209f43d06629a68b448f7072
boningggg <wang.boning@outlook.com>
2022-04-14 15:37:00 +0800

    [nvim] add c formatter

--- nvim/lua/plugin-config/formatter.lua
+++ nvim/lua/plugin-config/formatter.lua
@@ -1,22 +1,27 @@
-require("formatter").setup({
-  filetype = {
-    c = {
-      function()
-        return {
-          exe = "clang-format",
-          args = {
-            "--assume-filename=" .. vim.fn.expand("%:t"),
-            "-style='{\
+local cpp_style = {
+  function()
+    return {
+      exe = "clang-format",
+      args = {
+        "--assume-filename=" .. vim.fn.expand("%:t"),
+        "-style='{\
               BasedOnStyle: LLVM, \
               IndentWidth: 8, \
+              UseTab: Always, \
               IncludeBlocks: Regroup, \
+              BreakBeforeBraces: Linux, \
               AlignConsecutiveDeclarations: true, \
               AlignConsecutiveAssignments: true}'",
-          },
-          stdin = true,
-        }
-      end,
-    },
+      },
+      stdin = true,
+    }
+  end,
+}
+
+require("formatter").setup({
+  filetype = {
+    c = cpp_style,
+    cpp = cpp_style,
     java = {
       function()
         return {
