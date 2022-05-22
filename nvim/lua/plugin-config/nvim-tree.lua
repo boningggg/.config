63067cb5d1233dc1582bf1ecc6d7c0fa566ebf9b
boningggg <boningggg@boninggggs-MBP.modem>
2022-05-22 11:24:23 +1000

    update nvim config - nvim-tree - auto resize the tree by default

--- nvim/lua/plugin-config/nvim-tree.lua
+++ nvim/lua/plugin-config/nvim-tree.lua
@@ -24,24 +24,4 @@ nvim_tree.setup({
       error = "",
     },
   },
-  actions = {
-    use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = false,
-      restrict_above_cwd = false,
-    },
-    open_file = {
-      quit_on_open = false,
-      resize_window = true,
-      window_picker = {
-        enable = true,
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-        exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-          buftype = { "nofile", "terminal", "help" },
-        },
-      },
-    },
-  },
-  })
+})
